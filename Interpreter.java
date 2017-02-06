840fb5c4f6db2fccad06be5a2709ee57c22a71f5
→∟Paola Fuentes Caro← <Paola Fuentes Caro>
2017-02-05 21:29:02 -0600

    encabezado

--- Interpreter.java
+++ Interpreter.java
@@ -1,3 +1,8 @@
+/*
+Autor: Byron Andrés Mota Hernandez, 15246
+	   Paola Fuentes Caro, 151126
+Este se encarga de analizar y ejecutar el proceso de interpretar archivos postfix	   */
+	   
 public class Interpreter{
 
     public String Read(Nodo nodo){
--- Lector.java
+++ Lector.java
@@ -1,3 +1,9 @@
+/*
+Autor: Byron Andrés Mota Hernandez, 15246
+	   Paola Fuentes Caro, 151126
+Se encarga de recibir el archivo e instancia Cola para que se encargue de crear los nodos, segun las lineas que contenga el archivo.
+Tambien tiene una funcion de Main*/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -6,8 +12,9 @@ import java.io.IOException;
 public class Lector{
 
     public static void main(String[] arg){
-
+		
         Cola cola= new Cola();
+		/*obtiene el archivo*/
         Interpreter interpretador= new Interpreter();
         String archivo= "datos.txt";
         String[] resultados;
