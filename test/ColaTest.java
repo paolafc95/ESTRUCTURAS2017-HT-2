f536317abbbece50df691808fa3db980837ccdf6
→∟Paola Fuentes Caro← <Paola Fuentes Caro>
2017-02-05 23:39:24 -0600

    JUnit, Profiler, Javadoc y todo Finalizado

---
+++ test/ColaTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lilian Paola
+ */
+public class ColaTest {
+    
+    public ColaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
